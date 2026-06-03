4e20619d8e6a43e6d2c4bf54b189e8bc35b2ae5a
sharbaknra <muhammadsaadali2772.com>
2026-06-03 10:27:55 +0500

    Fix PORT to use Railway dynamic port

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import adminRoutes from './routes/admin.js'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json())
